44916fd72770d14d7b86a0c8ef91f64680c7ed20
Jean-Pierre <jp@mint-jp.fr>
2017-08-19 16:28:17 +0200

    Suppression de l'obfuscation du code

--- pi.c
+++ pi.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-long a=10000,b,c=8400,d,e,f[8401],g;
-main(){for(;b-c;)f[b++]=a/5;
-for(;d=0,g=c*2;c-=14,printf("%.4ld",e+d/a),e=d%a)
-for(b=c;d+=f[b]*a,f[b]=d%--g,d/=g--,--b;d*=b);
-return(0);}
+long a=10000;
+long b;
+long c=8400;
+long d;
+long e;
+long f[8401];
+long g;
+
+int main(int argc, char *argv[])
+{
+    for ( ;b-c; ) {
+        f[b++]=a/5;
+    }
+    for ( ;d=0, g=c*2; c-=14, printf("%.4ld", e+d/a), e = d%a) {
+       for (b = c; d+=f[b]*a, f[b] = d % --g, d /= g--, --b; d*=b);
+    }
+
+    return(0);
+}
