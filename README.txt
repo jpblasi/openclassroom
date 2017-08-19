2dac039ace6890480e9a94ff75e4568a57506f1a
Jean-Pierre <jp@mint-jp.fr>
2017-08-19 17:34:22 +0200

    Modification du nom des variables et réorganisation du code pour sa meilleure compréhension

--- README.txt
+++ README.txt
@@ -1,2 +1,5 @@
 # openclassroom
 Activité Partie 2
+Ce code C permet de calculer les décimales de PI.
+Il utilise le code fourni sur 'codes-sources'
+-> http://codes-sources.commentcamarche.net/source/34973-les-2400-premieres-decimales-de-pi
--- pi.c
+++ pi.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
-long a=10000;
-long b;
-long c=8400;
-long d;
-long e;
-long f[8401];
-long g;
+#define PREC 		10000
+#define TABLE_LEN	8400
+
+long i, j;
+
+long resultat;
+long retenue;
+long table[TABLE_LEN + 1];
+long tmp;
 
 int main(int argc, char *argv[])
 {
-    for ( ;b-c; ) {
-        f[b++]=a/5;
+    for (i = 0; i <= TABLE_LEN; ++i) {
+        table[i] = PREC / 5;
     }
-    for ( ;d=0, g=c*2; c-=14, printf("%.4ld", e+d/a), e = d%a) {
-       for (b = c; d+=f[b]*a, f[b] = d % --g, d /= g--, --b; d*=b);
+
+    for (j = TABLE_LEN / 14; j > 0; --j) {
+    	resultat = 0;
+    	for (i = j * 14; i > 0; --i) {
+    		tmp = (resultat * i) + (table[i] * PREC);
+    		resultat = tmp / ((2 * i) - 1);
+    		table[i] = tmp % ((2 * i) - 1);
+    	}
+    	printf("%.4ld", retenue + (resultat / PREC));
+    	retenue = resultat % PREC;
     }
 
     return(0);
