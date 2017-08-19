524317f6d218d1643e227bbe87e2213ee1b35791
Jean-Pierre <jp@mint-jp.fr>
2017-08-19 16:20:48 +0200

    Ajout du code source

---
+++ pi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+long a=10000,b,c=8400,d,e,f[8401],g;
+main(){for(;b-c;)f[b++]=a/5;
+for(;d=0,g=c*2;c-=14,printf("%.4ld",e+d/a),e=d%a)
+for(b=c;d+=f[b]*a,f[b]=d%--g,d/=g--,--b;d*=b);
+return(0);}
